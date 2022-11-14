054828d16dc99f9cfbde9bb3269de4332652a955
BENSAID Yacine <yacine.bensaid@soprasteria.com>
2022-11-14 12:00:59 +0100

    comienzo del proyecto

---
+++ src/main/java/com/example/proyectweb1/controllers/Controller1.java
@@ -0,0 +1,16 @@
+package com.example.proyectweb1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/page1")
+public class Controller1 {
+    @GetMapping("/tex1")
+public String getText1(){
+
+    return "page 1 text1";
+}
+
+}
